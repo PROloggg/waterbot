b8e89eacc6067fa7e05c465fa45e8bc455599c75
Sergey <s.noskov@nag.ru>
2026-03-17 09:48:41 +0500

    fix

--- EnvParser.php
+++ EnvParser.php
@@ -5,7 +5,12 @@ class EnvParser
     public static function parse(): array
     {
         $envList = [];
-        $text = file_get_contents('/app/waterbot/.env');
+        $envPath = '/app/waterbot/.env';
+        if (!is_file($envPath)) {
+            $envPath = __DIR__ . DIRECTORY_SEPARATOR . '.env';
+        }
+
+        $text = file_get_contents($envPath);
         $lines = explode("\n", trim($text));
 
         foreach ($lines as $line) {
